ca3649287419d72fe6183fd007c80e473c78edad
Malind Kowalski <malind.nikulski@gmail.com>
2016-12-19 15:58:44 -0700

    using index to play w/ shotgun

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,12 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, 'finstagram_mek'
   end
 
-  get "/" do
-    # erb :welcome
-    "welcome to finstagram"
+  get '/' do
+    erb :index
   end
 
 end
