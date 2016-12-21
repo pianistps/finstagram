ceb0d627fcea6d82d571f55150a7418963773f9b
Malind Kowalski <malind.nikulski@gmail.com>
2016-12-21 12:57:45 -0700

    actions for post/user cons, new/create_user/login

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,3 +1,34 @@
 class PostController < ApplicationController
 
+  get '/posts' do
+    if is_logged_in?(session)
+      @posts = Post.all
+      erb :'/posts/posts'
+    else
+      flash[:error] = "Please log in."
+      redirect '/login'
+    end
+  end
+
+  get '/posts/new' do
+    # "hello new post"
+    if is_logged_in?(session)
+      erb :'/posts/new'
+    else
+      flash[:error] = "Please log in"
+      redirect '/login'
+    end
+  end
+
+  post '/posts' do
+    if params[:post_photo] && params[:caption] != ""
+      Post.create(post_photo: params[:post_photo], caption: params[:caption], user_id: session[:user_id])
+      flash[:success] = "Post successfully made!"
+      redirect '/posts'
+    else
+      flash[:error] = "Please make sure both fields are present."
+      redirect '/posts/new'
+    end
+  end
+
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,24 @@
 class UserController < ApplicationController
 
+  get '/login' do
+    if !is_logged_in?(session)
+      erb :'/users/login'
+    else
+      redirect '/posts'
+    end
+  end
+
+  post '/login' do
+    user = User.find(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/posts'
+    else
+      flash[:error] = "Your login information seems to be incorrect."
+      redirect '/login'
+    end
+  end
+
   get '/signup' do
     # "Sign up"
     if !is_logged_in?(session)
@@ -10,16 +29,21 @@ class UserController < ApplicationController
   end
 
   post '/signup' do
-    @user = User.new(params[:user])
-    @user.user_photo = ""
-    session[:user_id] = @user.id
-    @user.save
-    if @user.save
+    if params.none? {|k, v| v == ""}
+      user = User.create(username: params[:username], learn_handle: params[:learn_handle], user_photo: params[:user_photo], password: params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Created successfully!"
       redirect '/posts'
     else
-      flash[:error] = "Error"
+      flash[:error] = "Unsuccessfully created."
       redirect '/signup'
     end
+  end
+
+  get '/logout' do
+    session.clear
+    flash[:success] = "See you next time!"
+    redirect '/login'
   end
 
 end
